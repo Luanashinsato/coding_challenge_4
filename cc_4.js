4a83865dca6ae32675b287bb6cff46669ac0b4cd
Luanashinsato <lshinsato@usf.edu>
2025-01-31 19:23:53 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,6 @@
+// Task 1: If Statements 
+let purchaseAmount = 180; // Declaring a variable for purchaseAmount 
+if (purchaseAmount > 100) {
+    purchaseAmount *= 0.9; 
+} // Applying a 10% discount if purchase amount is greater than 100
+console.log(`Final Amount After Discount: $${purchaseAmount}`); // Logging the final amount
