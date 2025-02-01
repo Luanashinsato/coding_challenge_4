8f7c6eb433aa2c0aefa525e768d3b0a803521412
Luanashinsato <lshinsato@usf.edu>
2025-01-31 20:05:17 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -4,3 +4,11 @@ if (purchaseAmount > 100) {
     purchaseAmount *= 0.9; 
 } // Applying a 10% discount if purchase amount is greater than 100
 console.log(`Final Amount After Discount: $${purchaseAmount}`); // Logging the final amount
+
+// Task 2: For Loop 
+let sales = [150, 82, 120, 50, 65]; // Declaring a sales array 
+let totalSales = 0; // Initializing totalSales variable
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+} // Using a for loop to calculate the total sales
+console.log(`Total sales: $${totalSales}`); // Logging the total sales
